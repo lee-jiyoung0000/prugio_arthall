fdb3239ae479af1647ff420bccd6018be19483b7
lee-jiyoung0000 <starseed300@kakao.com>
2023-06-23 11:32:16 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+// defer쓰고 윈도우값 구하는 것
+//  스크롤값과 윈도우넓이 높이값 구하는 거
+// 꼭 알고 있어야 함!!!!--> 중요!!
+
+// --> 윈도우값 알기!!!------------------------------
+// let winW = $(window).height();
+// console.log(winW);
+
+// // .: 체이닝 기법--> 갖고오기!!
+// // 돔구조의 main요소를 변수  mainEle에 담는다.
+// let mainEle = $('main');
+// console.log(mainEle);
+
+// // 돔요소 main에 높이값을 윈도우 높이값으로 대체한다.
+// mainEle.height(winW);
+// 제이쿼리 꼭 알고 있어라! 기본이다.
+// -----------------------------------------------------
+
+// 메쏘드~~
+// scrollTop(): 스크롤 높이값을 알기위해, width()-> 중요, height()-> 중요, addClass()--> 중요, removeClass()--> 중요
+// next()-> 중요, find()-> 첫번째 요소찾기, children(), parent(), parentNode()--> 부모의 부모.. 찾을때
+// , fadeIn(), fadeOut(),setInterval(),
+// show(), hide(), scrollDown()--> 스르륵 열리는 것, scrollUp()--> 스르륵......
+// scrollTo(): 스크롤 이동시
+// -if, for문, each문은 자바와 쿼리는 같다
+// - 위의 것들과 선택자는 꼭 알고 있어야 한다!!!
+// - 알고 있으면 원하는 거 다 만들수 있다.
+
+
+
+
+
+
+
